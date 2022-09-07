6db94c122a1982c80cff630d5d3f807bf6f80fb9
Supakron-Ch <knowshutdown@gmail.com>
2022-09-07 17:17:03 +0700

    members

--- app/python-project/myapp/members/views.py
+++ app/python-project/myapp/members/views.py
@@ -2,7 +2,14 @@ from re import template
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Members
+
 # Create your views here.
 def index(request):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    mymembers = Members.objects.all().values()
+    output = ""
+    for x in mymembers:
+        output += x["firstname"]
+
+    #template = loader.get_template('myfirst.html')
+    return HttpResponse(output)
