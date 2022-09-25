11bf98cbe7333cdfc892715c83389b7d4462cd8a
Supakron-Ch <knowshutdown@gmail.com>
2022-09-25 07:48:29 +0700

    Start fetching data from the database.

---
+++ app/python-project/myapp/category/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Category
+
+# Register your models here.
+
+admin.site.register(Category) # คำสั่งในการให้ admin จัดการกับ database นี้ได้
--- app/python-project/myapp/myapp/settings.py
+++ app/python-project/myapp/myapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from unicodedata import category
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'members.apps.MembersConfig',
-    'blogs'
+    'blogs',
+    'category',
 ]
 
 MIDDLEWARE = [
