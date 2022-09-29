0a7d3337293900bfc8c870b8dafe254ff363642d
Supakron-Ch <knowshutdown@gmail.com>
2022-09-29 19:30:52 +0700

    ทดสอบดึงของจาก database มาแสดงบนเว็ป

--- app/python-project/myapp/blogs/views.py
+++ app/python-project/myapp/blogs/views.py
@@ -2,10 +2,13 @@ from unicodedata import category
 from django.shortcuts import render
 from django.http import HttpResponse
 from category.models import Category
-
+from .models import Donation_blogs
 # Create your views here.
 
 def index(request):
     categories = Category.objects.all()
-    return render(request,"frontend/index.html",{'categories':categories})
+    blog = Donation_blogs.objects.all()
+    return render(request,"frontend/index.html",{'categories':categories,'blogs':blog})
+
+
    
