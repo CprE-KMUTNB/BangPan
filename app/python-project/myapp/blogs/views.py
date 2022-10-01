558d687456b27821a50a1652be3bbc71b8be5ff2
Supakron-Ch <knowshutdown@gmail.com>
2022-10-01 10:19:48 +0700

    การแสดงผลเลขหน้า

--- app/python-project/myapp/blogs/views.py
+++ app/python-project/myapp/blogs/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from category.models import Category
 from .models import Donation_blogs
+#ใช้สำหรับแบ่งแสดงข้อมูลในหนึ่งหน้า
+from django.core.paginator import Paginator,EmptyPage,InvalidPage
 # Create your views here.
 
 def index(request):
@@ -10,7 +12,21 @@ def index(request):
     blog = Donation_blogs.objects.all()
     #ให้เรียง primary key จากมากไปน้อย 
     latests = Donation_blogs.objects.all().order_by('-pk')[:2]
-    return render(request,"frontend/index.html",{'categories':categories,'blogs':blog,'latests':latests})
+    
+    #pagination
+    paginator = Paginator(blog,3)
+    try:
+        page = int(request.GET.get('page','1'))
+    except:
+        page = 1
+
+    try :
+        blogPrepage = paginator.page(page)
+    except (EmptyPage,InvalidPage):
+        # ถ้าไม่มีหริอเกิน จะให้คืนหน้า : จำนวนบทความทั้งหมด/3 
+        blogPrepage = paginator.page(paginator.num_pages)
+
+    return render(request,"frontend/index.html",{'categories':categories,'blogs':blogPrepage,'latests':latests})
 
 
    
