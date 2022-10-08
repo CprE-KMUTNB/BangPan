d8422065fe8d7b1e95ba68b5ee1a63ee3cabb87e
TIIIIIIW <109777077+TIIIIIIW@users.noreply.github.com>
2022-10-08 18:32:22 +0700

    test

--- learn-react/src/common/header/Search.jsx
+++ learn-react/src/common/header/Search.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import logo from '../assets/images/logo-test.svg'
+import { Link } from 'react-router-dom'
 
 const Search = () => {
   return (
     <>
-        <h1>Search</h1>
+        <section className='search'>
+          <div className='container c_flex'>
+            <div className='logo width'>
+              <img src={logo} alt="" />
+            </div>
+
+            <div className="search-box f_flex">
+              <i className='fa fa-search'></i>
+              <input type="text" placeholder='ค้นหา' />
+              <span>All Category</span>
+            </div>
+
+            <div className='icon f_flex width'>
+              <i className='fa fa-user icon_circle'></i>
+              <div className='cart'>
+                <Link to='/cart'>
+                  <i className='fa fa-shoping-bag icon-circle'></i>
+                  <span>0</span>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </section>
     </>
   )
 }
