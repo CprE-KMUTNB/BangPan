944e7bdbfa0a77cc1f7cad6b9c24868dfa9c4b37
TIIIIIIW <109777077+TIIIIIIW@users.noreply.github.com>
2022-10-09 00:36:24 +0700

    test

--- learn-react/src/common/header/Search.jsx
+++ learn-react/src/common/header/Search.jsx
@@ -5,29 +5,29 @@ import { Link } from 'react-router-dom'
 const Search = () => {
   return (
     <>
-        <section className='search'>
-          <div className='container c_flex'>
-            <div className='logo width'>
-              <img src={logo} alt="" />
-            </div>
+      <section className='search'>
+        <div className='container c_flex'>
+          <div className='logo width '>
+            <img src={logo} alt='' />
+          </div>
 
-            <div className="search-box f_flex">
-              <i className='fa fa-search'></i>
-              <input type="text" placeholder='ค้นหา' />
-              <span>All Category</span>
-            </div>
+          <div className='search-box f_flex'>
+            <i className='fa fa-search'></i>
+            <input type='text' placeholder='Search and hit enter...' />
+            <span>All Category</span>
+          </div>
 
-            <div className='icon f_flex width'>
-              <i className='fa fa-user icon_circle'></i>
-              <div className='cart'>
-                <Link to='/cart'>
-                  <i className='fa fa-shoping-bag icon-circle'></i>
-                  <span>0</span>
-                </Link>
-              </div>
+          <div className='icon f_flex width'>
+            <i className='fa fa-user icon-circle'></i>
+            <div className='cart'>
+              <Link to='/cart'>
+                <i className='fa fa-shopping-bag icon-circle'></i>
+                <span>0</span>
+              </Link>
             </div>
           </div>
-        </section>
+        </div>
+      </section>
     </>
   )
 }
