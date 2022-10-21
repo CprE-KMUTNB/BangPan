a641ea6adb6de306e4c64ba79b2febbd7150637e
TIIIIIIW <109777077+TIIIIIIW@users.noreply.github.com>
2022-10-21 22:26:06 +0700

    test

--- learn-react/src/components/list/SmallC.js
+++ learn-react/src/components/list/SmallC.js
@@ -1,14 +1,22 @@
 import React, { useState, useEffect } from 'react';
-//import axios from 'axios';
+import axios from 'axios';
 import { Link } from 'react-router-dom'
 
 const SmallC = () => {
     const [blogs, setBlogs] = useState([]);
     const [featuredBlog, setFeaturedBlog] = useState([]);
 
-    <div>
-        asd
-    </div>
+    useEffect (() => {
+        const fetchData = async () => {
+            try {
+                const res = awaith axios.get('${process.env.REACT_APP_API_URL}/api/blog/featured')
+            }
+            catch (err) {
+
+            }
+        }
+        fetchData();
+    })
 
 };
 
